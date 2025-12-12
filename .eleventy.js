8ec07a076b0d86a74df2fdb7155bffecd05baae4
Parbhat Kumar <prabhatpaswan028@gmail.com>
2025-12-12 12:50:28 +0530

    solved yml 404 error

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("script.js");
-  eleventyConfig.addPassthroughCopy("content/images");
+  eleventyConfig.addPassthroughCopy("content");
+  eleventyConfig.addPassthroughCopy("admin");
+
 
 
   return {

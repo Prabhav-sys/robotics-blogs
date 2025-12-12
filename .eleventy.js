1040762376a85e3a4c1d3cc1607a071f68302274
Parbhat Kumar <prabhatpaswan028@gmail.com>
2025-12-12 11:32:50 +0530

    11tyfied-the-static-pages

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.addPassthroughCopy("script.js");
+  eleventyConfig.addPassthroughCopy("content/images");
+
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+};
